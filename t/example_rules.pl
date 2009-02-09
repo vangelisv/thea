7f427ef19e188a192d6b0722b88b125efd36f08e
Chris Mungall <cjm@berkeleybop.org>
2009-02-09 15:34:58 -0800

    examples for SWRL

--- t/example_rules.pl
+++ t/example_rules.pl
@@ -1,6 +1,10 @@
 % use swrl.pl to convert these rules to a mixture of SWRL and OWL
+%
+% e.g.
+% swipl -g "[swrl],prolog_source_to_swrl_rules('t/example_rules.pl',Rules),maplist(writeln,Rules),maplist(swrl_to_owl_axioms,Rules,As),maplist(writeln,As)"
 
 % hasParent(?x1,?x2) ∧ hasBrother(?x2,?x3) ⇒ hasUncle(?x1,?x3) 
+% can be converted to a property chain
 hasUncle(X,Z) :- hasParent(X,Y), hasBrother(Y,Z).
 
 % role chain
