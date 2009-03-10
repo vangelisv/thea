d596fe1c75b6212227ceb2a1ea6cb52bb7f23c81
Chris Mungall <cjm@berkeleybop.org>
2009-03-10 02:46:30 -0500

    added missing axiom metadata

--- owl2_metamodel.pl
+++ owl2_metamodel.pl
@@ -20,6 +20,32 @@ owlpredicate_typed(unionOf,objectUnionOf).
 owlpredicate_typed(unionOf,dataUnionOf).
 owlpredicate_typed(oneOf,objectOneOf).
 owlpredicate_typed(oneOf,dataOneOf).
+owlpredicate_typed(hasValue,objectHasValue).
+owlpredicate_typed(hasValue,dataHasValue).
+owlpredicate_typed(minCardinality,objectMinCardinality).
+owlpredicate_typed(minCardinality,dataMinCardinality).
+owlpredicate_typed(maxCardinality,objectMaxCardinality).
+owlpredicate_typed(maxCardinality,dataMaxCardinality).
+owlpredicate_typed(exactCardinality,objectExactCardinality).
+owlpredicate_typed(exactCardinality,dataExactCardinality).
+
+owlpredicate_typed(functionalProperty,functionalObjectProperty).
+owlpredicate_typed(functionalProperty,functionalDataProperty).
+
+owlpredicate_typed(subPropertyOf,subObjectPropertyOf).
+owlpredicate_typed(subPropertyOf,subDataPropertyOf).
+owlpredicate_typed(disjointProperties,disjointObjectProperties).
+owlpredicate_typed(disjointProperties,disjointDataProperties).
+owlpredicate_typed(equivalentProperties,equivalentObjectProperties).
+owlpredicate_typed(equivalentProperties,equivalentDataProperties).
+
+owlpredicate_typed(propertyDomain,objectPropertyDomain).
+owlpredicate_typed(propertyDomain,dataPropertyDomain).
+owlpredicate_typed(propertyRange,objectPropertyRange).
+owlpredicate_typed(propertyRange,dataPropertyRange).
+
+owlpredicate_typed(propertyAssertion,objectPropertyAssertion).
+owlpredicate_typed(propertyAssertion,dataPropertyAssertion).
 
 owlpredicate_arguments(objectIntersectionOf,[list(classExpression)]).
 owlpredicate_arguments(dataIntersectionOf,[list(dataExpression)]).
@@ -33,6 +59,35 @@ owlpredicate_arguments(objectUnionOf,[list(classExpression)]).
 owlpredicate_arguments(dataUnionOf,[list(dataExpression)]).
 owlpredicate_arguments(objectOneOf,[list(individual)]).
 owlpredicate_arguments(dataOneOf,[list(dataRange)]).
+owlpredicate_arguments(objectHasValue,[objectPropertyExpression,individual]). % TODO
+owlpredicate_arguments(dataHasValue,[dataPropertyExpression,dataRange]).
+owlpredicate_arguments(objectMinCardinality,[int,objectPropertyExpression,classExpression]). % TODO: non-QCR
+owlpredicate_arguments(dataMinCardinality,[int,dataPropertyExpression,dataRange]). % TODO: non-QCR
+owlpredicate_arguments(objectMaxCardinality,[int,objectPropertyExpression,classExpression]). % TODO: non-QCR
+owlpredicate_arguments(dataMaxCardinality,[int,dataPropertyExpression,dataRange]). % TODO: non-QCR
+owlpredicate_arguments(objectExactCardinality,[int,objectPropertyExpression,classExpression]). % TODO: non-QCR
+owlpredicate_arguments(dataExactCardinality,[int,dataPropertyExpression,dataRange]). % TODO: non-QCR
+
+owlpredicate_arguments(inverseOf,[objectProperty]).
+
+owlpredicate_arguments(functionalObjectProperty,[objectPropertyExpression]).
+owlpredicate_arguments(functionalDataProperty,[dataPropertyExpression]).
+
+owlpredicate_arguments(dataPropertyDomain,[dataPropertyExpression, classExpression]).
+owlpredicate_arguments(objectPropertyDomain,[objectPropertyExpression, classExpression]).
+owlpredicate_arguments(dataPropertyRange,[dataPropertyExpression, dataRange]).
+owlpredicate_arguments(objectPropertyRange,[objectPropertyExpression, classExpression]).
+
+owlpredicate_arguments(subObjectPropertyOf,[objectPropertyExpressionOrChain, objectPropertyExpression]).
+owlpredicate_arguments(subDataPropertyOf,[dataPropertyExpression, dataPropertyExpression]).
+owlpredicate_arguments(disjointObjectProperties,[list(objectPropertyExpression)]).
+owlpredicate_arguments(disjointDataProperties,[list(dataPropertyExpression)]).
+owlpredicate_arguments(equivalentObjectProperties,[list(objectPropertyExpression)]).
+owlpredicate_arguments(equivalentDataProperties,[list(dataPropertyExpression)]).
+
+owlpredicate_arguments(annotationAssertion,[annotationProperty, iri, value]).
+owlpredicate_arguments(objectPropertyAssertion,[objectPropertyExpression, individual, individual]).
+owlpredicate_arguments(dataPropertyAssertion,[dataPropertyExpression, individual, literal]).
 
 
 /** <module> 
