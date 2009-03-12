0df6b5e53761c079d688d4b0a8775d512503335e
Chris Mungall <cjm@berkeleybop.org>
2009-03-11 18:58:31 -0700

    added more syntactic sugar

--- owl2_plsyn.pl
+++ owl2_plsyn.pl
@@ -1,7 +1,8 @@
 /* -*- Mode: Prolog -*- */
 
 :- module(owl2_plsyn,[
-                       op(1150,fx,class),
+                      op(1200,xfy,(--)),
+                      op(1150,fx,class),
  op(1150,fx,individual),
  op(1150,fx,transitive),
  op(1150,fx,symmetric),
@@ -20,8 +21,12 @@
 
                       ]).
 
+:- op(1200,xfy,(--)).
 :- op(1150,fx,class).
 :- op(1150,fx,individual).
+
+:- op(1150,xfy,disjointUnion).
+
 :- op(1150,fx,transitive).
 :- op(1150,fx,symmetric).
 :- op(1150,fx,asymmetric).
@@ -49,22 +54,58 @@ plsyn2owl(Pl,Owl) :-
         plpred2owlpred(PlPred,OwlPred),
         !,
         maplist(plsyn2owl,Args,Args2),
-        maplist(plsyn2owl,Args,Args2),
         Owl=..[OwlPred|Args2].
+plsyn2owl(Pl,Owl) :-
+        Pl=..[PlPred|Args],
+        plpred2owlpred_list(PlPred,OwlPred),
+        !,
+        maplist(plsyn2owl,Args,Args2),
+        Owl=..[OwlPred,[Args2]].
+
+% TODO: entity annotations
+plsyn2owl(Ax--Comments,[PlAx,axiomAnnotation('rdfs:comment',literal(Comments))]) :-
+        !,
+        plsyn2owl(Ax,PlAx).
+
+plsyn2owl(R < R1*R2,subPropertyOf(R,propertyChain(Chain))) :-
+        plsyn2owl_ec(R1*R2,(*),Chain).
+
+% we can chain over a=b=c=d as equivalent/sameAs is transitive
+% (note we cannot do this for different/disjoint)
+plsyn2owl(A=B,sameIndividual(ECs)) :-
+        !,
+        plsyn2owl_ec(A=B,(=),ECs).
+plsyn2owl(A==B,equivalentClasses(ECs)) :-
+        !,
+        plsyn2owl_ec(A==B,(==),ECs).
 plsyn2owl(X,X) :- !.
+
+
+plsyn2owl_ec(T,Op,L) :-
+        T=..[Op,A,B],
+        !,
+        plsyn2owl_ec(A,Op,LA),
+        plsyn2owl_ec(B,Op,LB),
+        append(LA,LB,L).
+plsyn2owl_ec(A,_,[A]).
+        
+
 
 owlsyn2pl(Owl,Pl) :-
         Owl=..[OwlPred|Args],
         plpred2owlpred(PlPred,OwlPred),
         !,
-        maowlist(owlsyn2pl,Args,Args2),
-        maowlist(owlsyn2pl,Args,Args2),
+        maplist(owlsyn2pl,Args,Args2),
         Pl=..[PlPred|Args2].
 owlsyn2pl(X,X) :- !.
 
 plpred2owlpred(transitive,transitiveProperty).
 
 plpred2owlpred(<,subClassOf). % TODO -- use for subpropertyof too
+
+plpred2owlpred_list(\=,differentIndividuals). 
+plpred2owlpred_list(\=,disjointClasses). 
+
 
 
 
