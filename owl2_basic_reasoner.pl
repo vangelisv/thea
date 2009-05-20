1679e1003b973b1fecc93391f1b1ada36b90442a
Chris Mungall <cjm@berkeleybop.org>
2009-05-20 01:20:55 -0700

    making simple reasoning more complete

--- owl2_basic_reasoner.pl
+++ owl2_basic_reasoner.pl
@@ -13,7 +13,8 @@
 
 %% entailed(?Axiom) is nondet
 % true if Axiom is entailed
-entailed(A) :- entailed(A,[]).
+entailed(A) :-
+        entailed(A,[]).
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 % level 1: subproperties
@@ -27,6 +28,10 @@ entailed(subPropertyOf(X,Y),_) :- subPropertyOf(X,Y).
 % transitivity of subprops
 entailed(subPropertyOf(X,Y),EL) :- subPropertyOf(X,Z),\+member(X<Z,EL),entailed(subPropertyOf(X,Y),[X<Z|EL]). % TODO: cycles
 
+entailed(subClassOfReflexive(X,Y), EL) :- entailed(subClassOf(X,Y), EL).
+entailed(subClassOfReflexive(X,X), _) :- nonvar(X).
+
+
 entailed(A,EL) :- entailed_2(A,EL).
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,12 +39,34 @@ entailed(A,EL) :- entailed_2(A,EL).
 %  
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-% 
+% X<Y if X=^DL and X<all D in DL
 entailed_2(subClassOf(X,Y),EL) :-
+        nonvar(Y),
         pairwise_equivalent_class(Y,intersectionOf(DL)),
         \+ member(X<Y,EL),
         forall(member(D,DL),
-               entailed_5(subClassOf(X,D),EL)).
+               entailed(subClassOf(X,D),EL)).
+
+entailed_2(subClassOf(X,Y),EL) :-
+        nonvar(Y),
+        nonvar(X),
+        Y=intersectionOf(DL),
+        \+ member(X<Y,EL),
+        forall(member(D,DL),
+               entailed(subClassOf(X,D),EL)).
+
+entailed_2(subClassOf(intersectionOf(DL),Y),EL) :-
+        nonvar(Y),
+        \+ member(intersectionOf(DL)<Y,EL),
+        member(D,DL),
+        entailed(subClassOfReflexive(D,Y)).
+
+entailed_2(subClassOf(someValuesFrom(P,X),someValuesFrom(P,Y)),EL) :-
+        entailed(subClassOf(X,Y),EL).
+
+entailed_2(subClassOf(X,Y),_) :-
+        pairwise_equivalent_class(X,intersectionOf(DL)),
+        member(Y,DL).
 
 entailed_2(subClassOf(X,Y),_) :-
         pairwise_equivalent_class(X,Y).
@@ -56,6 +83,31 @@ entailed_2(classAssertion(C,I),EL) :-
         propertyRange(P,C),
         entailed(propertyAssertion(P,_,I),EL).
 
+entailed_2(classAssertion(C,I),EL) :-
+        pairwise_equivalent_class(C,intersectionOf(DL)),
+        entailed_2(classAssertion(DL,I),EL).
+
+entailed_2(classAssertion(intersectionOf(DL),I),EL) :-
+        nonvar(DL),
+        entailed(individual(I)),
+        forall(member(D,DL),
+               entailed_2(classAssertion(D,I),EL)).
+
+entailed_2(classAssertion(someValuesFrom(P,Y),I),EL) :-
+        propertyAssertion(P,I,YI),
+        entailed(classAssertion(Y,YI),EL).
+
+entailed_2(classAssertion(Y,I),EL) :-
+        entailed(individual(I)),
+        pairwise_equivalent_class(Y,intersectionOf(DL)),
+        forall(member(D,DL),
+               entailed(classAssertion(D,I),EL)).
+
+entailed_2(individual(I),_) :-
+        propertyAssertion(_,I,_).
+entailed_2(individual(I),_) :-
+        propertyAssertion(_,_,I).
+
 
 % NEXT LEVEL
 entailed_2(A,EL) :- entailed_5(A,EL).
@@ -66,6 +118,12 @@ entailed_2(A,EL) :- entailed_5(A,EL).
 
 % asserted
 entailed_5(subClassOf(X,Y),_) :- subClassOf(X,Y).
+
+% asserted
+entailed_5(classAssertion(C,I),_) :- classAssertion(C,I).
+
+% asserted
+entailed_5(individual(I),_) :- individual(I).
 
 % transitivity of subclass
 entailed_5(subClassOf(X,Y),EL) :- subClassOf(X,Z),\+member(X<Z,EL),entailed(subClassOf(Z,Y),[X<Z|EL]). % TODO: cycles
@@ -84,8 +142,8 @@ entailed_5(subClassOf(X,someValuesFrom(P,Y)), EL) :-
 % UTIL
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-pairwise_equivalent_class(X,Y) :- equivalentClasses(L),member(X,L),member(Y,L).
-pairwise_disjoint_class(X,Y) :- disjointClasses(L),member(X,L),member(Y,L).
+pairwise_equivalent_class(X,Y) :- equivalentClasses(L),member(X,L),member(Y,L),X\=Y.
+pairwise_disjoint_class(X,Y) :- disjointClasses(L),member(X,L),member(Y,L),X\=Y.
 
 % TODO: make subClassOf nothing?
 unsatisfiable(X) :-
