2c080ee3255aabcf8a8c4467ff8a754091306725
Chris Mungall <cjm@berkeleybop.org>
2009-06-19 01:15:36 -0700

    http://blipkit.wordpress.com/2009/06/19/translating-between-logic-programs-and-owlswrl/

--- testfiles/processual_part_of.pl
+++ testfiles/processual_part_of.pl
@@ -3,12 +3,17 @@
 % relation is processual_part_of
 processual_part_of(P,W) :- part_of(P,W),process(W).
 
-% range of processual_part_of is processs
-%process(W) :- processual_part_of(_,P).
+% if part_of holds for an object, the specific
+% relation is spatial_part_of
+static_part_of(P,W) :- part_of(P,W),object(W).
 
 part_of(p1,p2).
 part_of(ob1,ob2).
+process(p1).
 process(p2).
+object(ob1).
 object(ob2).
 
 
+% range of processual_part_of is processs
+%process(W) :- processual_part_of(_,P).
