22f402af9867edffc4c54b9badb9382d3a562f8d
Chris Mungall <cjm@berkeleybop.org>
2009-06-26 14:55:36 -0700

    added method for making equivalentClass axioms from labels

--- owl2_util.pl
+++ owl2_util.pl
@@ -12,6 +12,8 @@
            prefix_IRIs/1,
            translate_IRIs/1,
            map_IRIs/3,
+           use_class_labels_as_synonyms/1,
+           class_label_synonym_axiom/2,
            write_ontology_summary/0,
            treeview/1,
            treeview/2
@@ -24,22 +26,32 @@
 :- use_module(owl2_xml).
 :- use_module(owl2_basic_reasoner).
 
+% @Deprecated
+% use owl2_io
 owl_parse_pro(F):-
         owl2_model:consult(F).
 
 
+% @Deprecated
+% use owl2_io
 owlx_file_to_prolog(F):-
         owl_parse_xml(F),
         write_owl_as_prolog.
 
+% @Deprecated
+% use owl2_io
 rdf_file_to_prolog(F):-
         owl_parse_rdf(F),
         write_owl_as_prolog.
 
+% @Deprecated
+% use owl2_io
 rdf_file_to_prolog(F,Opts):-
         owl_parse_rdf(F,Opts),
         write_owl_as_prolog.
 
+% @Deprecated
+% use owl2_io
 write_owl_as_prolog:-
         forall(axiompred(PS),
                write_axioms(PS)).
@@ -53,22 +65,37 @@ write_axioms(P/A):-
 write_axioms(H):-
         forall(H,format('~q.~n',[H])).
 
+% @Deprecated
+% use retract_all_axioms/0 in owl2_model
 retract_axioms :-
         findall(A,axiom(A),Axioms),
         maplist(retract,Axioms).
 
+%% remove_namespaces
+% calls translate_IRIs/1 with remove_ns/2 as argument,
+% which has the effect of stripping URI prefixes from all
+% names. Note that we cannot then write the results
+% straight out as RDF since our identifiers are no longer IRIs.
+% however, namespace-stripped simple atom identifiers are
+% useful when working with prolog.
+%
+% @see prefix_IRIs/1 for the converse operation
 remove_namespaces:-
         translate_IRIs(remove_ns).
 
-
-%% use_labels_for_IRIs/0
-% uses rdfs:label if available, if not uses local part of URI
-use_labels_for_IRIs:-
-        translate_IRIs(use_label_as_IRI).
-
+%% prefix_IRIs(+NS)
+% attaches a prefix to all names.
 prefix_IRIs(X):-
         translate_IRIs(prefix_IRI(X)).
 
+%% use_labels_for_IRIs/0
+% uses rdfs:label if available, if not uses local part of URI.
+% e.g.
+% if we have axioms like subClassOf('http://x.org#1','http://x.org#2'),
+% and label annotationAssertion/2 axioms, then this we be replaced by
+% subClassOf(human,animal) etc
+use_labels_for_IRIs:-
+        translate_IRIs(use_label_as_IRI).
 
 remove_ns(IRI,X) :-
         concat_atom([_,X],'#',IRI).
@@ -88,6 +115,10 @@ prefix_IRI(Pre,X,Y) :-
 prefix_IRI(_,X,X) :- !.
 
 
+%% translate_IRIs(+Goal)
+% Goal must be a 2 argument predicate Goal(+IRI,?TranslatedAtom)
+% all IRIs are translated. The result need not be a valid IRI, it
+% can be any prolog atom (possibly compound term as well)
 :- module_transparent translate_IRIs/1.
 translate_IRIs(Goal):-
         findall(A,axiom(A),Axioms),
@@ -110,6 +141,25 @@ map_IRIs(G,X,X2) :-
 map_IRIs(G,X,X2) :-
         call(G,X,X2),
         !.
+
+%% use_class_labels_as_synonyms(+NS)
+% sometimes ontologies have opaque numeric IRIs.
+% sometimes it is convenient to work with a knowledge base in
+% which the labels are used. We can get the reasoner to combine
+% these by stating equivalentClasses/2 axioms.
+use_class_labels_as_synonyms(NS) :-
+        forall(labelAnnotation_value(IRI,X),
+               (   atom_concat(NS,X,C),
+                   assert_axiom(class(C)),
+                   assert_axiom(equivalentClasses([C,IRI])))).
+
+class_label_synonym_axiom(NS,Ax) :-
+        entity(IRI),
+        labelAnnotation_value(IRI,X),
+        atom_concat(NS,X,C),
+        (   Ax=class(C)
+        ;   Ax=equivalentClasses([C,IRI])).
+
 
 
 write_ontology_summary:-
