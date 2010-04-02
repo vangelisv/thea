dbdefb7195d20a1dce9eaf04009325e868589ac9
Chris Mungall <cjm@berkeleybop.org>
2010-04-02 10:49:21 -0700

    fix for missing APs

--- owl2_export_rdf.pl
+++ owl2_export_rdf.pl
@@ -110,6 +110,10 @@ owl2_export_axiom(objectProperty(C1),main_triple(C,'rdf:type','owl:ObjectPropert
         translate_iri(C1,C),
 	owl_rdf_assert(C,'rdf:type','owl:ObjectProperty'),!.
 
+owl2_export_axiom(annotationProperty(C1),main_triple(C,'rdf:type','owl:AnnotationProperty')) :-
+        translate_iri(C1,C),
+	owl_rdf_assert(C,'rdf:type','owl:AnnotationProperty'),!.
+
 owl2_export_axiom(subClassOf(C1,C2),main_triple(TC1,'rdfs:subClassOf',TC2)) :-
 	owl2_export_axiom(C1,main_triple(TC1,_,_)),
 	owl2_export_axiom(C2,main_triple(TC2,_,_)),
