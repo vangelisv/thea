cf04981399a79552b90611b3010a27cd8681b8a3
Chris Mungall <cjm@berkeleybop.org>
2010-05-03 22:29:11 -0700

    catalogs

--- owl2_catalog.pl
+++ owl2_catalog.pl
@@ -1,10 +1,24 @@
 % * -*- Mode: Prolog -*- */
 
 :- module(owl2_catalog,
-	  []).
+	  [
+	   register_ontology_localpath/2,
+	   save_catalog/0,
+	   save_catalog/1,
+	   load_catalog/0,
+	   load_catalog/1
+	  ]).
 
 :- use_module(library(sgml)).
 
+:- dynamic ontology_localpath/2.
+
+register_ontology_localpath(URL,Local) :-
+	retractall(ontology_localpath(URL,Local)),
+	assert(ontology_localpath(URL,Local)).
+
+owl2_from_rdf:owl_repository_hook(URL,Local) :-
+	ontology_localpath(URL,Local).
 owl2_from_rdf:owl_repository_hook(URL,Local) :-
 	exists_file('catalog.xml'),
 	url_local(URL,Local).
@@ -15,4 +29,18 @@ url_local(URL,LocalExpanded) :-
 	member(element(_,[name=URL,local=Local],_),Elts),
 	atom_concat(Base,Local,LocalExpanded).
 
-	
+save_catalog :-
+	save_catalog('catalog.pl').
+
+save_catalog(F) :-
+	tell(F),
+	forall(ontology_localpath(A,B),
+	       format('~q.~n',[ontology_localpath(A,B)])),
+	told.
+
+load_catalog :-
+	load_catalog('catalog.pl').
+
+load_catalog(F) :-
+	consult(F).
+
