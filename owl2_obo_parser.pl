80c7dbb592362c9e09c957599a190cc0d41d5183
Chris Mungall <cjm@berkeleybop.org>
2011-07-10 14:20:58 -0700

    wrapper for reading/writing obo

--- owl2_obo_parser.pl
+++ owl2_obo_parser.pl
@@ -22,6 +22,7 @@ owl_parse_obo_syntax_file(File,Opts) :-
         debug(obo,'obo2owl ~w ==> ~w URI: ~w',[File,OwlFile,OwlURI]),
         obo2owl(File,OwlFile,OwlURI,Opts).
 
+%% obo2owl(+In,+Out,URI,Opts) :-
 obo2owl(In,Out,URI,Opts) :-
         \+ exists_file(Out),
         !,
@@ -35,12 +36,12 @@ obo2owl(In,Out,_URI,Opts) :-
         \+ member(force(true),Opts),
         !,
         debug(obo,'~w < ~w -- using existing owl file',[InTime,OutTime]),
-        load_axioms(Out,owl,Opts).
+        load_axioms(Out,rdf_direct,Opts).  % USE NEW PARSER
 obo2owl(In,Out,URI,Opts) :-
         run_obo2owl(In,Out,URI,Opts).
 
 run_obo2owl(In,Out,URI,Opts) :-
         concat_atom(['obolib-obo2owl --default-ontology default --allow-dangling','-o',URI,In,'>/dev/null'],' ',Cmd),
         shell(Cmd),
-        load_axioms(Out,owl,Opts).
+        load_axioms(Out,rdf_direct,Opts). % USE NEW PARSER
 
