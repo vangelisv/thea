779cac084e8539f44ced2ada0a65a51ec4a117f2
Jan Wielemaker <J.Wielemaker@vu.nl>
2019-08-08 11:00:08 +0200

    Make thea.pl set the file search path

--- thea.pl
+++ thea.pl
@@ -1,3 +1,9 @@
+:- prolog_load_context(directory, Dir),
+   file_directory_name(Dir, Lib),
+   (   user:file_search_path(library, Lib)
+   ->  true
+   ;   asserta(user:file_search_path(library, Lib))
+   ).
 
 :-use_module('owl2_model.pl').
 :-use_module('owl2_from_rdf.pl').
